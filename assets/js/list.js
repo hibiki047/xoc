72df88359a3728c356fb9b6ebb8751dad4404419
neko <mingliang2234@gmail.com>
2023-06-24 12:29:43 +0800

    icon

---
+++ assets/js/list.js
@@ -0,0 +1,36 @@
+var links = [
+    {
+        url: "https://xoc.pages.dev/",
+        description: "Database",
+        logo: "./assets/images/logo/tamoe.png"
+    }
+]
+
+function generate_html_from_link(data) {
+    var content_row = document.createElement("div");
+    //Title
+    var row_title_container = document.createElement("div");
+    var row_title = document.createElement("a");
+    row_title.classList.add("link", "underline", "grid-link");
+    row_title.href = data.url;
+    row_title_container.appendChild(row_title);
+    //logo get(if any)
+    if (data.logo) {
+        row_title.innerText = "";
+        var row_title_logo = document.createElement(Image);
+        row_title_logo.classList.add("link", "underline", "link-logo");
+        row_title_logo.src = data.logo;
+        row_title_logo.alt = data.description;
+        row_title_logo.title = "".concat(data.url, ": ").concat(data.description);
+        row_title.appendChild(row_title_logo);
+    }
+
+    content_row.appendChild(row_title_container);
+    return content_row;
+}
+
+var general = document.getElementById("grid-general");
+
+for (var i = 0; i < links.length; i++) {
+    general === null || general === void 0 ? void 0 : general.appendChild(generate_html_from_link(links[i]));
+}
